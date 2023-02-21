b23a92c85428283f8900326fbdd37bc9e2394d53
cinavujgandra <124698817+cinavujgandra@users.noreply.github.com>
2023-02-22 00:17:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,151 @@
 let square = document.querySelectorAll('.square');
+let playerResult = document.querySelector('.playerResult');
+let computerResult = document.querySelector('.computerResult');
+let drawResult = document.querySelector('.drawResult');
 const combinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [7, 5, 3], [1, 5, 9]];
 let player = []
 let computer = []
+let playerResultCounter = 0
+let computerResultCounter = 0
+let drawResultCounter = 0
+let checkEqual = false;
 
 
-
-for (let i = 0; i < square.length; i++) {
-    square[i].onclick = () => {
-
-        square[i].innerHTML = "O"
-
-        if (square[i].dataset.bool == "true") {
-            square[i].dataset.bool = "false"
-            square[i].onclick = () => { return false }
-        }
-
-        let arrComb = []
-
-
-        for (let iter = 0; iter < square.length; iter++) {
-            if (square[iter].dataset.bool == 'true') {
-                arrComb = [...arrComb, iter]
-                
-            }
-
-        }
-
-
-        function getRandomItem(arr) {
-
-            // get random index value
-            const randomIndex = Math.floor(Math.random() * arr.length);
-
-            // get random item
-            const item = arr[randomIndex];
-
-            return item;
-        }
-
-
-        const result = getRandomItem(arrComb);
-        square[result].innerHTML = "X"
-        square[result].dataset.bool = "false"
-        square[result].onclick = () => { return false }
-
-        const index = arrComb.indexOf(result);
-
-        const x = arrComb.splice(index, 1);
-        arrComb = [...arrComb]
-
-        console.log(arrComb);
-
-
-
-        // win combinations
-        let dataValue = parseInt(square[i].dataset.value)
-        player = [...player, dataValue]
-
-        for (let a = 0; a < combinations.length; a++) {
-            let hasAllElems = combinations[a].every(elem => player.includes(elem));
-            if (hasAllElems == true) {
-                // console.log('Winner')
-                // console.log(combinations[a]);
-
-                break;
-            }
-
-        }
-
-    };
-
+const clearTable = () => {
+    player = [];
+    computer = [];
+    arrComb = [];
+    for (let i = 0; i < square.length; i++) {
+        square[i].onclick = () => { return true }
+        square[i].dataset.bool = "true"
+        square[i].innerHTML = ""
+        square[i].classList.remove("bg-success");
+        square[i].classList.remove("bg-danger");
+        start()
+    }
 }
+
+const desibleSquare = () => {
+    for (let i = 0; i < square.length; i++) {
+        square[i].onclick = () => { return false }
+    }
+}
+
+const start = () => {
+    for (let i = 0; i < square.length; i++) {
+
+        square[i].onclick = () => {
+
+            let arrComb = []
+
+            square[i].innerHTML = "X"
+
+            // clicked square
+            if (square[i].dataset.bool == "true") {
+                square[i].dataset.bool = "false"
+                square[i].onclick = () => { return false }
+            }
+
+            // get all unmarked squares
+            for (let iter = 0; iter < square.length; iter++) {
+                if (square[iter].dataset.bool == 'true') {
+                    arrComb = [...arrComb, iter]
+                }
+            }
+
+
+            if (arrComb.length == 0) {
+                checkEqual = true
+                arrComb = [parseInt(square[i].dataset.value) - 1]
+
+
+            } else {
+                function getRandomItem(arr) {
+
+                    // get random index value
+                    const randomIndex = Math.floor(Math.random() * arr.length);
+
+                    // get random item
+                    const item = arr[randomIndex];
+
+                    return item;
+                }
+
+                // computer combinations
+                const result = getRandomItem(arrComb);
+                square[result].innerHTML = "O"
+                square[result].dataset.bool = "false"
+                square[result].onclick = () => { return false }
+                const index = arrComb.indexOf(result);
+                arrComb.splice(index, 1);
+                computer = [...computer, result + 1]
+                arrComb = [...arrComb]
+            }
+
+            // player combinations
+            let dataValue = parseInt(square[i].dataset.value)
+            player = [...player, dataValue]
+
+
+            let playerElements
+            let computerElements
+            // winning combination
+
+            for (let a = 0; a < combinations.length; a++) {
+                playerElements = combinations[a].every(elem => player.includes(elem));
+                computerElements = combinations[a].every(elem => computer.includes(elem));
+
+                // player win
+                if (playerElements == true || playerElements == true && checkEqual == true) {
+                    desibleSquare()
+                    checkEqual = false
+                    playerResultCounter++
+                    playerResult.innerHTML = playerResultCounter
+
+                    for (let win = 0; win < combinations[a].length; win++) {
+                        square[combinations[a][win] - 1].classList.add("bg-success");
+
+                    }
+                    setTimeout(function () {
+                        clearTable()
+                    }, 1000);
+                    break;
+                }
+
+                // computer win
+                if (computerElements == true) {
+                    desibleSquare()
+                    computerResultCounter++
+                    computerResult.innerHTML = computerResultCounter
+                    for (let win = 0; win < combinations[a].length; win++) {
+                        square[combinations[a][win] - 1].classList.add("bg-danger");
+
+                    }
+                    setTimeout(function () {
+                        clearTable()
+                    }, 1000);
+
+                    break;
+                }
+            }
+
+            // draw
+            if (checkEqual == true && playerElements == false) {
+                drawResultCounter++
+                drawResult.innerHTML = drawResultCounter;
+                checkEqual = false
+                setTimeout(function () {
+                    clearTable()
+                }, 1000);
+
+            }
+
+
+        };
+
+    }
+}
+
+
+start()
 
